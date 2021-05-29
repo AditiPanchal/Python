4d521433d3cc27b29da52ad75848e4a2639159a3
AditiPanchal <83535649+AditiPanchal@users.noreply.github.com>
2021-05-29 16:44:35 +0530

    Add files via upload

---
+++ positive numbers in a range.py
@@ -0,0 +1,13 @@
+l1=[]
+n=int(input("enter no. of elements of list:"))
+for i in range (0,n):
+    a=int(input())
+    l1.append(a)
+print("the list is:",l1) 
+l2=[]
+for i in l1:
+    if i>0:
+        b=i
+        l2.append(b)
+print("the list will all positive integers is:",l2)  
+
